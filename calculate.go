b8b71422b107e120564d9d235a50b046b441e0a5
singgihdwindaru <singgihlab@gmail.com>
2023-04-15 03:16:40 +0700

    add comment function

--- calculate.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func Calculate(x, y int) int {
-	return x + y
-}
---
+++ math/calculate.go
@@ -0,0 +1,21 @@
+// Package math provide mathematical functions
+package math
+
+// Calculate simple plus math
+func Calculate(x, y int) int {
+	return x + y
+}
+
+// Calculate simple plus math
+//
+// function description with line breaker (an enter)
+func CalculateV2(x, y int) int {
+	return x + y
+}
+
+// Calculate simple plus math
+//
+// Deprecated: udah usang
+func Plus(x, y int) int {
+	return x + y
+}
